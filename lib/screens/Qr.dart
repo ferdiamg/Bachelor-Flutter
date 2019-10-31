397a3569f4d9ff0c2ed5a64cac01f3f27054a83c
ferdiamg <ferdi.armagan@stud.hs-hannover.de>
2019-10-31 14:49:08 +0100

    Implemented QR code scanning for iOS and Android

--- lib/screens/Qr.dart
+++ lib/screens/Qr.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
 
-class Qr extends StatelessWidget {
+class Qr extends StatefulWidget {
+  @override
+  _QrState createState() => _QrState();
+}
+
+class _QrState extends State<Qr> {
+  String result = "";
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+      });
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          result = "Camera permission was denied";
+        });
+      } else {
+        setState(() {
+          result = "Unknown Error $ex";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = "You pressed the back button before scanning anything";
+      });
+    } catch (ex) {
+      setState(() {
+        result = "Unknown Error $ex";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,7 +55,7 @@ class Qr extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(top: 5.0),
-              child: Text("Scanning will start automatically",
+              child: Text(this.result,
                   style: TextStyle(
                     color: Color(0xCFCFCF).withOpacity(1),
                     fontSize: 16,
@@ -28,6 +64,11 @@ class Qr extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(top: 80.0),
               child: SvgPicture.asset('assets/qrmarker.svg', height: 290),
+            ),
+            FloatingActionButton.extended(
+              icon: Icon(Icons.camera_alt),
+              label: Text("Scan"),
+              onPressed: _scanQR,
             ),
           ],
         ),
